554facd5613b0e1a3666c7f580d73ce60d5d2b76
DivyaKiran2001 <divyakirantatikonda@gmail.com>
2024-04-24 22:56:58 +0530

    register and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!:FormGroup
 
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.registerForm=this.fb.group(
+      {
+        username:['',Validators.required],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(6)]]
+      }
+    )
+  };
+
+  onSubmit()
+  {
+    console.log(this.registerForm);
+  }
 }
