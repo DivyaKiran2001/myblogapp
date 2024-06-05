0418f994159e35b56cde8ff753b084c4b313d17e
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2024-06-05 11:21:06 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'homepage',pathMatch: 'full'},
+  {path: 'login',component:LoginComponent ,canActivate:[] },
+  {path: 'signup',component: RegisterComponent,canActivate:[] },
+  {path: 'homepage',component: HomePageComponent}
+  {path: 'user/blogs/:id' ,component: BlogComponent},
+  {path: 'user/blogs/addblog' ,component: AddBlogComponent},
+  {path: 'user/blogs/editblog' ,component: EditComponent},
+  {path: 'user/blogs/deleteblog' ,component: DeleteComponent}, 
+  {path: 'user/blogs/viewblogs' ,component: BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
